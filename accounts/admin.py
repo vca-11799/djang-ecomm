9180437e14adb2b93142426c12bc1c9889cf9560
vca-11799 <vishalagravat555@gmail.com>
2024-03-17 15:05:21 +0530

    this is first commit

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . models import Account
+
+# Register your models here.
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email','first_name','last_name')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(Account,AccountAdmin)
---
+++ ecomm/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from store.models import Product
+
+
+def home(request):
+    
+    products = Product.objects.all().filter(is_available = True)
+
+    context = {
+            'products' : products,
+    }
+
+    return render(request,'index.html', context)
+
