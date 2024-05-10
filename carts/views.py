9f8a52db1045bc56469e13e7930e1203815135c7
vca-11799 <vishalagravat555@gmail.com>
2024-05-10 20:15:08 +0530

    store and cart functionality

--- carts/views.py
+++ carts/views.py
@@ -51,22 +51,27 @@ def add_cart(request, product_id):
 
         if product_variation in ex_var_list:
             #increasing cart qty
-            index = ex_var_list.index(product_variation)
+            index = ex_var_list.index(product_variation)    
             item_id = id[index]
             item = CartItem.objects.get(product=product, id=item_id)
             item.quantity += 1
             item.save()
 
         else: 
+            item = CartItem.objects.create(product=product, quantity=1, cart=cart)
             if len(product_variation) > 0:
-                cart_item.variation.clear()
-                cart_item.variations.add(*product_variation)
-                cart_item.save()
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            item.save()
     else:
-        item = CartItem.objects.create(product = product,quantity = 1,cart = cart)
+        cart_item = CartItem.objects.create(
+                product = product,
+                quantity = 1,
+                cart = cart,
+            )
         if len(product_variation) > 0:
-            item.variations.clear()
-            item.variations.add(*product_variation)
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     return redirect('carts')
 
@@ -104,10 +109,10 @@ def decrease_cart(request, cart_item_id):
     return redirect('carts')
 
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id = _cart_id(request))
     product = get_object_or_404(Product, id = product_id)
-    cart_item = CartItem.objects.get(product = product, cart = cart)
+    cart_item = CartItem.objects.get(product = product, cart = cart, id = cart_item_id)
     cart_item.delete()
 
     return redirect('carts')
