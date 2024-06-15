3c9ed3df711045ad749dff96a06518f322014268
vca-11799 <vishalagravat555@gmail.com>
2024-06-15 21:36:02 +0530

    paypal integration

---
+++ orders/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('place_order/', views.place_order, name = 'place_order'),
+    path('payments/', views.payments, name = 'payments'),
+] 
